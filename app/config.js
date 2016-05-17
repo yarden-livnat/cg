62844097961b7ccab58e50ba69471b2e4fb16a05
Yarden Livnat <yarden@sci.utah.edu>
2016-05-17 10:01:21 -0600

    update packages

--- app/config.js
+++ app/config.js
@@ -1,77 +1,84 @@
 System.config({
-  "baseURL": "/",
-  "transpiler": "babel",
-  "babelOptions": {
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
     "optional": [
       "runtime"
     ]
   },
-  "paths": {
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js",
-    "*": "*.js"
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   },
-  "shim": {
+  shim: {
     "bootstrap-multiselect": {
       "deps": [
         "jquery"
       ]
     }
   },
-  "defaultJSExtensions": true
-});
 
-System.config({
-  "map": {
-    "babel": "npm:babel-core@5.8.34",
-    "babel-runtime": "npm:babel-runtime@5.8.34",
+  map: {
+    "babel": "npm:babel-core@5.8.38",
+    "babel-runtime": "npm:babel-runtime@5.8.38",
     "bootstrap": "github:twbs/bootstrap@3.3.6",
-    "bootstrap-multiselect": "github:davidstutz/bootstrap-multiselect@0.9.13",
     "core-js": "npm:core-js@1.2.6",
     "crossfilter": "github:square/crossfilter@1.3.12",
-    "d3": "github:mbostock/d3@3.5.9",
+    "d3": "github:d3/d3@3.5.17",
     "davidstutz/bootstrap-multiselect": "github:davidstutz/bootstrap-multiselect@0.9.13",
     "firstopinion/formatter.js": "github:firstopinion/formatter.js@0.1.5",
-    "font-awesome": "npm:font-awesome@4.4.0",
+    "font-awesome": "npm:font-awesome@4.6.1",
     "formatter.js": "github:firstopinion/formatter.js@0.1.5",
-    "jquery": "github:components/jquery@2.1.4",
+    "jquery": "github:components/jquery@2.2.1",
     "leaflet": "github:Leaflet/Leaflet@0.7.7",
     "lockr": "github:tsironis/lockr@0.8.2",
     "lodash": "npm:lodash@3.10.1",
-    "mbostock/queue": "github:mbostock/queue@1.0.7",
-    "postal": "npm:postal@1.0.7",
+    "mbostock/queue": "github:d3/d3-queue@1.2.3",
+    "postal": "npm:postal@1.0.11",
     "queue": "github:mbostock/queue@1.0.7",
     "square/crossfilter": "github:square/crossfilter@1.3.12",
-    "tsironis/lockr": "github:tsironis/lockr@0.8.3",
+    "tsironis/lockr": "github:tsironis/lockr@0.8.4",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
+    },
+    "github:jspm/nodelibs-buffer@0.1.0": {
+      "buffer": "npm:buffer@3.6.0"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
     },
     "github:jspm/nodelibs-process@0.1.2": {
-      "process": "npm:process@0.11.2"
+      "process": "npm:process@0.11.3"
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
     "github:twbs/bootstrap@3.3.6": {
-      "jquery": "github:components/jquery@2.1.4"
+      "jquery": "npm:jquery@2.2.3"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.34": {
+    "npm:babel-runtime@5.8.38": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:buffer@3.6.0": {
+      "base64-js": "npm:base64-js@0.0.8",
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "ieee754": "npm:ieee754@1.1.6",
+      "isarray": "npm:isarray@1.0.0",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:core-js@1.2.6": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
     },
-    "npm:font-awesome@4.4.0": {
-      "css": "github:systemjs/plugin-css@0.1.20"
+    "npm:font-awesome@4.6.1": {
+      "css": "github:systemjs/plugin-css@0.1.21"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
@@ -79,13 +86,17 @@ System.config({
     "npm:lodash@3.10.1": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
+    "npm:lodash@4.12.0": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:postal@1.0.7": {
-      "lodash": "npm:lodash@3.10.1"
+    "npm:postal@1.0.11": {
+      "lodash": "npm:lodash@4.12.0"
     },
-    "npm:process@0.11.2": {
+    "npm:process@0.11.3": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
     "npm:util@0.10.3": {
@@ -94,4 +105,3 @@ System.config({
     }
   }
 });
-
