282823d293c5f953f0a36d050e2dce028b72d545
Yarden Livnat <yarden@sci.utah.edu>
2016-05-22 12:07:06 -0600

    upgrade libraries

--- app/config.js
+++ app/config.js
@@ -25,7 +25,7 @@ System.config({
     "bootstrap": "github:twbs/bootstrap@3.3.6",
     "core-js": "npm:core-js@1.2.6",
     "crossfilter": "github:square/crossfilter@1.3.12",
-    "d3": "github:d3/d3@3.5.17",
+    "d3": "npm:d3@3.5.17",
     "davidstutz/bootstrap-multiselect": "github:davidstutz/bootstrap-multiselect@0.9.13",
     "firstopinion/formatter.js": "github:firstopinion/formatter.js@0.1.5",
     "font-awesome": "npm:font-awesome@4.6.1",
@@ -40,7 +40,7 @@ System.config({
     "square/crossfilter": "github:square/crossfilter@1.3.12",
     "tsironis/lockr": "github:tsironis/lockr@0.8.4",
     "github:jspm/nodelibs-assert@0.1.0": {
-      "assert": "npm:assert@1.3.0"
+      "assert": "npm:assert@1.4.0"
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.6.0"
@@ -55,13 +55,20 @@ System.config({
       "util": "npm:util@0.10.3"
     },
     "github:twbs/bootstrap@3.3.6": {
-      "jquery": "npm:jquery@2.2.3"
+      "jquery": "npm:jquery@2.2.4"
     },
-    "npm:assert@1.3.0": {
+    "npm:assert@1.4.0": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "buffer-shims": "npm:buffer-shims@1.0.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "util": "npm:util@0.10.3"
     },
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:buffer-shims@1.0.0": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
     "npm:buffer@3.6.0": {
       "base64-js": "npm:base64-js@0.0.8",
